0abbe4cfb5fc68f2bdce8abfb72565b6ab7cea1a
Razila <razila3@gmail.com>
2017-04-10 13:05:04 -0700

    -added qmap layer -bubble chart and binned points based on qmap

--- SeattleCrimeMap.R
+++ SeattleCrimeMap.R
@@ -10,6 +10,8 @@ devtools::install_github("dkahle/ggmap")
 ##SET MAP PARAMETERS
 seattle<-get_map(location = "Seattle", zoom = 12)
 
+Seattle<-qmap("seattle",zoom = 14, color = "color")
+
 
 ##LOAD SPD CRIME NUMBERS
 SeattleCrimes<-read.csv("Seattle_Police_Department_Police_Report_Incident.csv")
@@ -23,8 +25,20 @@ SeattleCrimeMap<-subset(SeattleCrimes, Summarized.Offense.Description %in% c("AS
                                                             "BURGLARY-SECURE PARKING-",
                                                             "NARCOTICS",
                                                             "PROSTITUTION",
-                                                            "ROBBERY"))
+                                                           "ROBBERY"))
+
+
 ###PLOT SPD NUMBERS ONTO MAP
+##POINTS
 ggmap(seattle)+
   geom_point(data = SeattleCrimeMap, 
              aes(x=Longitude, y=Latitude, colour = Summarized.Offense.Description))
+
+###BUBBLE CHART
+Seattle+geom_point(data = SeattleCrimeMap,
+                   aes(x = Longitude, y=Latitude, colour = Summarized.Offense.Description,
+                       size = Summarized.Offense.Description))
+###BINNED POINTS
+Seattle+stat_bin2d(data = SeattleCrimeMap,
+                   aes(x=Longitude,y=Latitude,colour = Summarized.Offense.Description,fill = Summarized.Offense.Description),
+                   size = .5, bins = 30, alpha = 1/2)
